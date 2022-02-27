6ec57eda11d469a24b8c16ba6a05c23cfe0b09a4
jwam-create <77247458+jwam-create@users.noreply.github.com>
2022-02-27 16:37:04 -0500

    Setup project and added bootstrap

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+	return (
+		<div className="App">
+			<h1>Hello from Dictionary App</h1>
+			<button className="btn btn-primary">Submit</button>
+		</div>
+	);
+}
