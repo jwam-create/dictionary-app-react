f0bf7bb043eb463be9a42416c64761f924cb0d21
jwam-create <77247458+jwam-create@users.noreply.github.com>
2022-02-27 16:46:00 -0500

    Added sunset image to test adding a picture to react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Sunset from "./Sunset.PNG";
 
 export default function App() {
 	return (
 		<div className="App">
 			<h1>Hello from Dictionary App</h1>
 			<button className="btn btn-primary">Submit</button>
+			<br />
+			<img src={Sunset} alt="picture of a sunset" />
 		</div>
 	);
 }
