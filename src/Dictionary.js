9f976d88c2f73321b2f2c53710f929582408c48c
jwam-create <77247458+jwam-create@users.noreply.github.com>
2022-03-09 14:48:25 -0500

    Add axios and API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
 	const [value, setValue] = useState(null);
+	const [data, setData] = useState("");
 
-	function handleSubmit(event) {
-		event.preventDefault();
+	function handleResponse(response) {
+		setData({
+			word: response.data[0].word,
+			phonetic: response.data[0].phonetics[0].text,
+			noun: response.data[0].meanings[0].definitions[0].definition,
+			verb: response.data[0].meanings[1].definitions[0].definition,
+		});
+		console.log(response.data[0]);
+		console.log(response.data[0].word);
 	}
-	function search(event) {}
+
+	function search(event) {
+		event.preventDefault();
+
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
+		axios.get(apiUrl).then(handleResponse);
+	}
+	function handleSearchInput(event) {
+		setValue(event.target.value);
+	}
 
 	return (
 		<div className="Dictionary">
-			<form className="d-inline-flex" onSubmit={handleSubmit}>
+			<form className="d-inline-flex" onSubmit={search}>
 				<input
 					type="search"
 					placeholder="Search for a word"
