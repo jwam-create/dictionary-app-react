9ac9ca78c3aa90d4dc0eb061e0537348da6919d8
jwam-create <77247458+jwam-create@users.noreply.github.com>
2022-03-14 15:29:24 -0400

    Added text and audio phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,7 +3,6 @@ import Examples from "./Examples";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-	console.log(props.meaning);
 	return (
 		<div className="Meaning">
 			<h4>{props.meaning.partOfSpeech} </h4>
--- src/Results.js
+++ src/Results.js
@@ -2,11 +2,24 @@ import React from "react";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
+	console.log(props.results.phonetics);
 	if (props.results) {
 		return (
 			<div className="Results">
 				<h2 className="text-capitalize">{props.results.word}</h2>
-				<h4>{props.results.phonetics[0].text}</h4>
+				<h4>
+					{props.results.phonetics.map(function (phonetics, index) {
+						return (
+							<div key={index}>
+								<a href={phonetics.audio} target="_blank">
+									Listen
+								</a>
+								{phonetics.text}
+							</div>
+						);
+					})}
+				</h4>
+
 				{props.results.meanings.map(function (meaning, index) {
 					return (
 						<div key={index}>
