128c4968e115594b90262fe690fd9596293428d2
jwam-create <77247458+jwam-create@users.noreply.github.com>
2022-03-17 16:27:35 -0400

    Add loaded state and default keyword

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,17 +3,16 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-	const [value, setValue] = useState(null);
+export default function Dictionary(props) {
+	const [value, setValue] = useState(props.defaultKeyword);
 	const [data, setData] = useState(null);
+	const [loaded, setLoaded] = useState(false);
 
 	function handleResponse(response) {
 		setData(response.data[0]);
 	}
 
-	function search(event) {
-		event.preventDefault();
-
+	function search() {
 		// documentation: https://dictionaryapi.dev/
 		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
 		axios.get(apiUrl).then(handleResponse);
@@ -21,22 +20,36 @@ export default function Dictionary() {
 	function handleSearchInput(event) {
 		setValue(event.target.value);
 	}
+	function handleSubmit(event) {
+		event.preventDefault();
+		search();
+	}
 
-	return (
-		<div className="Dictionary">
-			<section>
-				<form className="d-inline-flex" onSubmit={search}>
-					<input
-						type="search"
-						placeholder="Type a word"
-						autoFocus={true}
-						className="form-control"
-						onChange={handleSearchInput}
-					/>
-				</form>
-				<p className="hint">Suggestions: sunset, cat, yoga, forest...</p>
-			</section>
-			<Results results={data} />
-		</div>
-	);
+	function load() {
+		setLoaded(true);
+		search();
+	}
+
+	if (loaded) {
+		return (
+			<div className="Dictionary">
+				<section>
+					<form className="d-inline-flex" onSubmit={handleSubmit}>
+						<input
+							type="search"
+							placeholder="Type a word"
+							autoFocus={true}
+							className="form-control"
+							onChange={handleSearchInput}
+						/>
+					</form>
+					<p className="hint">Suggestions: sunset, cat, yoga, forest...</p>
+				</section>
+				<Results results={data} />
+			</div>
+		);
+	} else {
+		load();
+		return null;
+	}
 }
